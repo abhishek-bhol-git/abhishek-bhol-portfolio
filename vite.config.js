8c69913518ef74d4bb98920a489f3fddb2a414dd
Abhishek Bhol <38851119+iamabhishekbhol@users.noreply.github.com>
2026-05-24 13:10:19 +0530

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const defaultGitHubPagesBase = '/abhishek-portfolio/';
+const defaultGitHubPagesBase = '/abhishek-bhol-portfolio/';
 
 export default defineConfig({
   plugins: [react()],
